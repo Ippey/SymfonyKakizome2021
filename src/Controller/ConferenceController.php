5ac716084ced3c9a0d08711f9747d7cc17878f4f
Ippei Sumida <ippey.s@gmail.com>
2021-01-25 18:22:17 +0900

    step13

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -21,7 +21,7 @@ class ConferenceController extends AbstractController
             'conferences' => $conferences,
         ]);
     }
-    #[Route('/show/{id}', name: 'conference')]
+    #[Route('/show/{slug}', name: 'conference')]
     public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
     {
         $offset = max(0, $request->query->getInt('offset', 0));
